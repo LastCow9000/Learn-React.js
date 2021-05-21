d7483fdac40bd2f882d956510a9680cb93126620
LastCow9000 <sys19912002@hanmail.net>
2021-05-22 01:19:24 +0900

    voca customHooks apply

--- README.md
+++ README.md
@@ -68,3 +68,9 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `yarn build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+### how to run json-server
+
+turn on terminal And...
+
+> json-server --watch ./src/db/data.json --port 3001
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from 'react';
+import useFetch from '../hooks/useFetch';
 
 export default function DayList() {
+  const days = useFetch("http://localhost:3001/days");
 
-  const [days, setDays] = useState([]);
+  /* 커스텀 훅을 사용하여 위 한줄로 대체 */
+  // const [days, setDays] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:3001/days")
-      .then(res => {
-        return res.json()
-      })
-      .then(data => {
-        setDays(data);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:3001/days")
+  //     .then(res => {
+  //       return res.json()
+  //     })
+  //     .then(data => {
+  //       setDays(data);
+  //     });
+  // }, []);
 
   return (
     <ul className="list_day">
